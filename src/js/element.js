f5f14c93974bb7c39fe7bbe6163799127abc05d8
Matthias DELON <matthias.delon@etu.umontpellier.fr>
2020-02-13 14:34:09 +0100

    Début structure de base

---
+++ src/js/element.js
@@ -0,0 +1,20 @@
+class Element {
+
+    /**
+     * @param x : int coordonnée x
+     * @param y : int coordonnée y
+     * @param id : String type d'élément
+     * @param str : String chemin relatif vers la source du sprite
+     */
+    constructor(x,y,id,str){
+        this.coordX = x;
+        this.coordY = y;
+        this.id = is;
+        this.sprite = document.getElementById(this.id);
+    }
+
+    placer() {
+        this.sprite.style.top = ``
+    }
+
+}
---
+++ src/js/niveau.js
@@ -0,0 +1,14 @@
+class Niveau {
+
+    constructor(num) {
+        this.num = num;
+        this.grid = [];
+        for (let i = 0; i < 20; i++) {
+            let ligne = [];
+            for (let j = 0; j < 20; j++) {
+                //TODO créer un élément en se basant sur un fichier csv représentant un niveau
+            }
+        }
+    }
+
+}
